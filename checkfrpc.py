d22033a13cae88caf329560ff69e4783b751c9e6
longway.xu <1985974121@qq.com>
2023-05-16 19:53:00 +0800

    update

--- checkfrpc.py
+++ checkfrpc.py
@@ -2,9 +2,9 @@ import os
 import time
 import threading
 
-def check_frpc(to_list:bool = False)->bool:
+def check_frpc(to_list=False):
     
-    shell = "sudo systemctl status frpc@frpc"
+    shell = "systemctl status frpc@frpc"
     if "active (running)" in get_shell_output(shell,to_list):
         return True
     return False
@@ -12,29 +12,31 @@ def check_frpc(to_list:bool = False)->bool:
 
 def restart_frpc():
     
-    shell = "sudo systemctl restart frpc@frpc"
+    shell = "systemctl restart frpc@frpc"
     out = get_shell_output(shell,False)
     return 
 
 
 def stop_frpc():
 
-    shell = "sudo systemctl stop frpc@frpc"
+    shell = "systemctl stop frpc@frpc"
     _ = get_shell_output(shell,False)
     return
 
 
 def start_frpc():
 
-    shell = "sudo systemctl start frpc@frpc"
+    shell = "systemctl start frpc@frpc"
     _ = get_shell_output(shell,False)
     return 
 
 
-def get_shell_output(shell:str,to_list:bool =True):
+def get_shell_output(shell,to_list=True):
     fd = os.popen(shell)
+    print shell
     out = fd.read()
     fd.close()
+    print out
     if to_list:
         out = out.split()
     return out
@@ -43,11 +45,14 @@ def action(check_epoch, sleep_time, *args, **kwargs):
     # time how long you are looking forward to sleep (minutes)
     for i in range(check_epoch):
         while not check_frpc(False):
+            print check_frpc(False)
             restart_frpc()
+            print check_frpc(False)
         time.sleep(sleep_time*60)
     return 
 
 if __name__ == '__main__':
-    # start_frpc()
+    start_frpc()
+    # print "Original frpc@frpc running",check_frpc(False)
     action(check_epoch=5, sleep_time=5)
     
--- checkfrpc/checkfrpc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=/usr/lib/python3/dist-packages
-# export PATH=$PATH:
-sudo python3 /home/linaro/checkfrpc.py
